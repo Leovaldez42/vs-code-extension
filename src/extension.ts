aa03ef1abcc69949834aa363b8f79e1f8a035f52
Leovaldez42 <gaurav4.037@gmail.com>
2021-01-27 11:19:18 +0530

    added ask question

--- src/extension.ts
+++ src/extension.ts
@@ -2,9 +2,27 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "vstodo" is now active!');
-	let disposable = vscode.commands.registerCommand('vstodo.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World!');
-	});
-	context.subscriptions.push(disposable);
+	
+	context.subscriptions.push
+	(	vscode.commands.registerCommand('vstodo.helloWorld', () => {
+		vscode.window.showInformationMessage('Hello World!'); 
+	
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand("vstodo.askQuestion", async () => {
+			const answer = await vscode.window.showInformationMessage(
+				"How was your day?", 
+				"good", 
+				"bad"
+			);
+			if (answer === "bad" ) {
+				vscode.window.showInformationMessage("Sorry to hear that");
+			} else {
+				console.log({ answer });
+			}
+		})
+	);
 }
 export function deactivate() {}
